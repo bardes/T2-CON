d00edfbd71e6a427119def27eb1b412e0c15574b
Paulo Bardes <paulobardes@ig.com.br>
2015-11-09 16:14:33 -0200

    Fix blur

--- blur.c
+++ blur.c
@@ -5,85 +5,69 @@
 
 #include "utils.h"
 
+#define PXM_MEM_LIMIT (1024 * 0x100000)
+#define MIN(x, y) (x < y ? x : y)
+#define MAX(x, y) (x > y ? x : y)
+
 static double clamp(double d, double min, double max) {
-    const double t = d < min ? min : d;
-    return t > max ? max : t;
+    return MIN(MAX(d, min), max);
 }
 
-static void x_blur_line_8(uint8_t *pixels, size_t w,
-        size_t radius, float *blur_buffer)
+static void x_blur_line_8(uint8_t *src, float *dest, size_t w, size_t radius)
 {
     /* Calculates the average of the first pixel */
-    blur_buffer[0] = pixels[0];
+    dest[0] = (radius + 1) * src[0];
     for (size_t i = 1; i <= radius; ++i)
-        blur_buffer[0] += pixels[i]; 
-    blur_buffer[0] /= (radius + 1);
+        dest[0] += src[i]; 
+    dest[0] /= (2 * radius + 1);
 
     /* And now calculates the average of the next ones */
     for (size_t p = 1; p < w; ++p) {
-        blur_buffer[p] = blur_buffer[p - 1];
-        float next = pixels[p + radius > w ? p + w - 1 : p + radius];
-        float prev = pixels[p < radius ? 0 : p - radius];
-        blur_buffer[p] += (next - prev) / ((2 * radius) + 1);
+        dest[p] = dest[p - 1];
+        float next = src[p + radius < w ? p + radius : p + radius];
+        float prev = src[p <= radius ? 0 : p - radius - 1];
+        dest[p] += (next - prev) / ((2 * radius) + 1);
     }
 }
 
-static void x_blur_line_16(uint16_t *pixels, size_t w,
-        size_t radius, float *blur_buffer)
+static void y_blur_line_8(float *src, uint8_t *dest, float *line_buffer,
+        size_t w, size_t rup, size_t rdw)
 {
-    /* Calculates the average of the first pixel */
-    blur_buffer[0] = pixels[0];
-    for (size_t i = 1; i <= radius; ++i)
-        blur_buffer[0] += pixels[i]; 
-    blur_buffer[0] /= (radius + 1);
+    memcpy(line_buffer, src, w * sizeof(float));
 
-    /* And now calculates the average of the next ones */
-    for (size_t p = 1; p < w; ++p) {
-        blur_buffer[p] = blur_buffer[p - 1];
-        float next = p + radius > w ? 0 : pixels[p + radius];
-        float prev = p < radius     ? 0 : pixels[p - radius];
-        blur_buffer[p] += (next - prev) / ((2 * radius) + 1);
-    }
-}
+    size_t n = 1;
+    for (size_t r = 1; r <= rup; ++r, ++n)
+        for (size_t p = 0; p < w; ++p)
+            line_buffer[p] += src[-(w * r) + p]; 
 
-static void y_blur_line_8(PXM_Image *img, size_t line,
-        size_t radius, float *blur_buffer)
-{
-   if (line < radius) {
-   } else if ((line + radius) >= img->len) {
-   } else {
-       for (size_t p = 0; p < img->w; ++p) {
-           float acc = blur_buffer[img->w * line + p];
-           for (size_t r = 1; r <= radius; ++r) {
-               acc += blur_buffer[img->w * (line - r) + p];
-               acc += blur_buffer[img->w * (line + r) + p];
-           }
-           ((uint8_t*) img->pixels)[img->w * line + p] = 
-               (uint8_t) clamp(acc / (2 * radius + 1), 0, 255);
-       }
-   }
+    for (size_t r = 1; r <= rdw; ++r, ++n)
+        for (size_t p = 0; p < w; ++p)
+            line_buffer[p] += src[w * r + p]; 
+
+    for (size_t p = 0; p < w; ++p)
+        dest[p] = (uint8_t) clamp(line_buffer[p] / n, 0, 255);
 }
 
 void PXM_blur(PXM_Image *img, size_t radius)
 {   
+    FAIL_MSG(img->max < 256,, "16 bit blur not implemented!");
     float *blur_buffer = (float*) malloc(img->w * img->len * sizeof(float));
+    float *line_buffer = (float*) malloc(img->len * sizeof(float));
     FAIL(blur_buffer,);
 
-    if (img->max > 255) {
-        for (size_t line = 0; line < img->len; ++line) {
-            x_blur_line_16(((uint16_t*) img->pixels) + line * img->w,
-                        img->w, radius, blur_buffer + line * img->w);
-        }
-    } else {
-        for (size_t line = 0; line < img->len; ++line) {
-            x_blur_line_8(((uint8_t*) img->pixels) + line * img->w,
-                        img->w, radius, blur_buffer + line * img->w);
-        }
-        for (size_t line = 0; line < img->len; ++line) {
-            y_blur_line_8(img, line, radius, blur_buffer);
-        }
+    for (size_t line = 0; line < img->len; ++line) {
+        x_blur_line_8(((uint8_t*)img->pixels) + line * img->w,
+                blur_buffer + line * img->w, img->w, radius);
+    }
+
+    for (size_t line = 0; line < img->len; ++line) {
+        y_blur_line_8(blur_buffer + line * img->w,
+                ((uint8_t*)img->pixels) + line * img->w,
+                line_buffer, img->w, MIN(line, radius),
+                MIN(img->len - line, radius));
     }
 
     free(blur_buffer);
+    free(line_buffer);
 }
 
